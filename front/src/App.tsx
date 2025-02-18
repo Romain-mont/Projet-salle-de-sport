06c9eb04fc1386c749fe7293dc93d4de7f70d53d
Romain-mont <romainmontagnon26200@gmail.com>
2025-02-18 18:01:54 +0100

    add component carousel and navbar

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,18 +1,13 @@
-import { Button } from "flowbite-react";
+import { ComponentCarousel } from "./components/Carousel/Carousel";
+import { NavbarSport } from "./components/Navbar/Navbar";
+
 
 function App() {
   return (
-<div className="flex flex-wrap gap-2">
-      <Button>Default</Button>
-      <Button color="blue">Blue</Button>
-      <Button color="gray">Gray</Button>
-      <Button color="dark">Dark</Button>
-      <Button color="light">Light</Button>
-      <Button color="success">Success</Button>
-      <Button color="failure">Failure</Button>
-      <Button color="warning">Warning</Button>
-      <Button color="purple">Purple</Button>
-    </div>
+    <>
+    <NavbarSport/>
+    <ComponentCarousel/>
+    </>
 )
 }
 
